fce729119d7cebaba5c6f105b23101539c5622b9
jose <jose.celis@appspringtech.com>
2021-04-23 18:39:25 -0400

    includes effect for desappearing nav bar after three seconds and other sections

--- js/app.js
+++ js/app.js
@@ -1,342 +1,184 @@
+import {createNavbar, createCard, createPlayerCard, getInitialHeights, 
+    getResizeHeights, getCurrentSection, scrollToSection} from './functions.js';
+import {navbar_items, score_details, score_card_classes,
+    schedule_details, schedule_card_classes, players_details,
+    player_card_classes} from './data.js';
+
+
 /*Navbar*/
-/*Navbar items declarions: menu items texts + links */
-navbar_items = [
-    {'item_name': 'Scores', 'link': '#', 'class':'navbar-item'},
-    {'item_name': 'Team', 'link': '#', 'class':'navbar-item'},
-    {'item_name': 'Schedule', 'link': '#', 'class':'navbar-item'},
-    {'item_name': 'Live', 'link': '#', 'class':'navbar-item'},
-    {'item_name': 'Store', 'link': '#', 'class':'navbar-item'},
-];
 
-const fragment = new DocumentFragment();
 const item = document.getElementById('navbar-list-items');
-let counter = 1;
+createNavbar(navbar_items, item);
 
-navbar_items.forEach(navbar_item => {
-    //creates <li> 
-    const new_item = document.createElement('li');
-    new_item.innerHTML = `<a href="${navbar_item.link}">${navbar_item.item_name}</a>`;
-    new_item.classList.add(navbar_item.class);
-    new_item.setAttribute("id", `navbar-item-${counter}`);
-    counter ++; 
-    fragment.appendChild(new_item);
-});
+//Creates Score section
+const score_container = document.getElementById('score-container');
+createCard (score_details, score_card_classes, score_container);
 
-item.appendChild(fragment);
+//Creates Schedule section
+const schedule_container = document.getElementById('schedule-container');
+createCard (schedule_details, schedule_card_classes, schedule_container);
+
+//Creates Team section
+const team_container = document.getElementById('team-container');
+createPlayerCard(players_details, player_card_classes, team_container);
+
+
 
 /*Navbar Locator*/
 const vp_height = window.innerHeight;
-const sections_position_list = new Array();
 
 //Gettiing navbar height
 const navbar = document.querySelector('header');
 const navbar_height = navbar.getBoundingClientRect().bottom;
+
+//Getting active section
+let is_scrolling;
+window.addEventListener('scroll', e =>{
+
+    console.log('ejecuntado scroll');
+    getCurrentSection(vp_height, navbar_height, sections);
+
+    navbar.classList.remove('hide-element');
+
+    if (window.pageYOffset === 0){
+        console.log('top');
+        //Removing styles from other sections
+        document.querySelectorAll('.underlined-navbar-item')
+        .forEach(other_item => other_item.classList.remove('underlined-navbar-item'));
+
+    }
+
+    //Detects if user stopped scrolling
+    window.clearTimeout(is_scrolling);
+
+    is_scrolling = setTimeout(() =>{
+            console.log('Scroll stopped');
+
+            setTimeout(() =>{
+                if (window.pageYOffset != 0){
+                    console.log('hiding bar');
+                    navbar.classList.add('hide-element');
+                }
+            }, 2000);
+    }, 1000);
+
+    
+});
+
+
+//Selects all sections
 const sections = document.querySelectorAll('h2');
 
-//Getting element heights after DOM is loades
-document.addEventListener('DOMContentLoaded', e =>{
-    
-    sections.forEach(section => {
+let sections_position_list = 0;
+let sections_position_list_initial = 0;
 
-        const section_position = {
-            'section_text': '',
-            'section_y': '',
-        };
-
-        const section_text = section.textContent;
-        const section_y = section.getBoundingClientRect().bottom;
-        section_position.section_text = section_text;
-        section_position.section_y = section_y;
-        sections_position_list.push(section_position); 
-    });
-
-    //Setting inicial section
-    document.getElementById('navbar-item-1').classList.add('underlined-navbar-item');
+//Getting element heights after Window is loaded
+window.addEventListener('load', e =>{
+    sections_position_list = getInitialHeights(sections);
+    sections_position_list_initial = sections_position_list 
     console.log(sections_position_list);
-    // console.log(sections_position_list.length);
 });
 
-//Getting element heights after resize
+// window.addEventListener('scroll', e =>{
+//     sections_position_list = getInitialHeights(sections);
+//     console.log(sections_position_list);
+
+//     // if (e.target.id === "top-log"){
+//     //     document.querySelectorAll('.underlined-navbar-item')
+//     //     .forEach(other_item => other_item.classList.remove('underlined-navbar-item'));
+//     //     console.log('jjj');
+//     // }
+// });
+
+//Getting element heights if viewport is resized
 window.addEventListener('resize', e =>{
-    console.log('hello');
-    let counter_2 = 0;
-    sections.forEach(section => {
-        sections_position_list[counter_2].section_y = section
-        .getBoundingClientRect().bottom;
-        counter_2 ++;
-    });
-    console.log(sections_position_list);
+    sections_position_list = getResizeHeights(sections_position_list, sections);
+    console.log(sections_position_list);    
 });
 
-//Getting current section
-document.addEventListener('scroll', e =>{
-    
-    counter = 1;
-    sections.forEach(section => {
-        const section_y_current = (section.getBoundingClientRect().bottom);
-        if (section_y_current >= navbar_height && section_y_current <= (vp_height/3)){
-            
-            // //Removing styles from other sections
-            document.querySelectorAll('.underlined-navbar-item')
-            .forEach(other_item => other_item.classList.remove('underlined-navbar-item'));
+console.log(sections_position_list);
 
-            //Setting current section style
-            const navbar_item = document.getElementById(`navbar-item-${counter}`);
-            navbar_item.classList.add('underlined-navbar-item');
-            counter = 1;
-        }
-        counter ++;
-    });
+//Smooth Scrooling to sections
+const navbar_list = document.getElementById('navbar-list-items');
+navbar_list.addEventListener('click', e =>{
+    e.preventDefault();
+    sections_position_list = getInitialHeights(sections);
+    scrollToSection(sections_position_list, navbar_height, e.target.textContent);
+    //sections_position_list = getInitialHeights(sections);
 });
 
-//Smooth Scrooling
-document.addEventListener('DOMContentLoaded', () =>{
+//Smooth Scrooling to top
+const logo = document.getElementById('main-logo');
+logo.addEventListener('click', e =>{
 
-    const items = document.querySelectorAll('.navbar-item');
-    console.log(items);
+    window.scrollTo(0, 0);
+    console.log(e);
+//.forEach(other_item => other_item.classList.remove('underlined-navbar-item'));
+});
+
+
+// //Smooth Scrooling
+// document.addEventListener('DOMContentLoaded', () =>{
+
+//     const items = document.querySelectorAll('.navbar-item');
+//     console.log(items);
 
     
-    items.forEach( item => {
-        item.addEventListener('click', e => {
+//     items.forEach( item => {
+//         item.addEventListener('click', e => {
 
-            e.preventDefault;
-            console.log(e.target.textContent);
+//             e.preventDefault;
+//             console.log(e.target.textContent);
 
-            let offset_y = 0;
+//             let offset_y = 0;
 
-            sections_position_list.forEach(section => {
-                if (e.target.textContent === section.section_text){
-                    offset_y = section.section_y;
-                    console.log(offset_y);
-                }
-            });
+//             sections_position_list.forEach(section => {
+//                 if (e.target.textContent === section.section_text){
+//                     offset_y = section.section_y;
+//                     console.log(offset_y);
+//                 }
+//             });
 
-            setTimeout(() =>{
-                console.log(sections_position_list);
-                console.log(offset_y);
-                offset_y = offset_y - navbar_height*2;
-                item.classList.add('underlined-navbar-item');
-                window.scrollBy({
-                    top: offset_y,
-                    left: 0,
-                    behavior: 'smooth',
-                });
-            }, 0.001);
+//             setTimeout(() =>{
+//                 console.log(sections_position_list);
+//                 console.log(offset_y);
+//                 offset_y = offset_y - navbar_height*2;
+//                 item.classList.add('underlined-navbar-item');
+//                 window.scrollBy({
+//                     top: offset_y,
+//                     left: 0,
+//                     behavior: 'smooth',
+//                 });
+//             }, 0.0001);
 
-            let counter_1 = 0;
+//             let counter_1 = 0;
 
-            setTimeout(() =>{
-                sections.forEach(section => {
-                    // console.log(section
-                    //     .getBoundingClientRect().bottom);
-                    // console.log(counter_1);
-                    sections_position_list[counter_1].section_y = section
-                    .getBoundingClientRect().bottom;
-                    counter_1 ++;
-                    // console.log(counter_1);
-                });
-                console.log(sections_position_list);
+//             setTimeout(() =>{
+//                 sections.forEach(section => {
+//                     // console.log(section
+//                     //     .getBoundingClientRect().bottom);
+//                     // console.log(counter_1);
+//                     sections_position_list[counter_1].section_y = section
+//                     .getBoundingClientRect().bottom;
+//                     counter_1 ++;
+//                     // console.log(counter_1);
+//                 });
+//                 console.log(sections_position_list);
 
-            }, 1000);
+//             }, 1000);
 
             
-        });  
+//         });  
         
 
 
 
-    });
+//     });
 
-});
-
-
+// });
 
 
 
 
 
 
-/*Scores*/
-/*Scores from previous matches*/
-score_details = [
-    {'date': 'Final', 
-    'score_local':'108',
-    'score_visit':'111',
-    'logo_local': './media/MIA-viceversa.png',
-    'logo_visit': './media/LAL.png',
-    'location': 'Miami'
-    },
-    {'date': 'Tuesday - April 27th, 2021', 
-    'score_local':'-',
-    'score_visit':'-',
-    'logo_local': './media/LAL.png',
-    'logo_visit': './media/MIA-viceversa.png',
-    'location': 'Denver'
-    },
-    {'date': 'Friday - April 30th, 2021', 
-    'score_local':'-',
-    'score_visit':'-',
-    'logo_local': './media/LAL.png',
-    'logo_visit': './media/MIA-viceversa.png',
-    'location': 'Chicago'
-    },
-];
-
-game_card_classes = {
-    'game_card_conteiner': [`match-score`, `game-card`, `flex`],
-    'game_date': [`match-date`],
-    'game_score_detail': [`score-detail`, `flex`],
-    'game_location': [`match-location`, `flex`],
-}
-
-const fragment_score = new DocumentFragment();
-const score_container = document.getElementById('score-container');
-
-
-score_details.forEach(score_detail => {
-
-    //Game Card creation
-    const game_card = document.createElement('div');
-    game_card.classList.add(...game_card_classes.game_card_conteiner);
-
-    //Appenidng date
-    const game_date = document.createElement('div');
-    game_date.classList.add(...game_card_classes.game_date);
-    game_date.textContent = score_detail.date;
-    game_card.appendChild(game_date);
-
-    //Appending Score and team logos
-    const game_score_container = document.createElement('div');
-    game_score_container.classList.add(...game_card_classes.game_score_detail);
-
-    //---> Scores
-    const game_score_local = document.createElement('span');
-    game_score_local.textContent = score_detail.score_local;
-    const game_score_visit = document.createElement('span');
-    game_score_visit.textContent = score_detail.score_visit;
-    const game_score_divider = document.createElement('span');
-    game_score_divider.textContent = '-';
-
-    //---> Logos
-    const game_logo_local = document.createElement('img');
-    game_logo_local.setAttribute('src', score_detail.logo_local);
-    const game_logo_visit = document.createElement('img');
-    game_logo_visit.setAttribute('src', score_detail.logo_visit);
-
-    game_score_container.appendChild(game_logo_local)
-    game_score_container.appendChild(game_score_local)
-    game_score_container.appendChild(game_score_divider)
-    game_score_container.appendChild(game_score_visit)
-    game_score_container.appendChild(game_logo_visit);
-
-    game_card.appendChild(game_score_container);
-
-    // Appending Location
-    const game_location = document.createElement('div');
-    game_location.classList.add(...game_card_classes.game_location);
-    game_location.textContent = score_detail.location;
-
-    game_card.appendChild(game_location);
-
-    //Appending all components
-    fragment_score.appendChild(game_card)
-});
-
-score_container.appendChild(fragment_score);
-
-
-/*Team*/
-/*Team Players Info*/
-players_details = [
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-    {'name': 'Nemanja Bjelica', 
-    'position':'Center-Forward',
-    'number':'70',
-    'player_pic': './media/players/roster-vv-1628389.png',
-    },
-];
-
-player_card_classes = {
-    'player_card_container': [`player-card`,  `flex`],
-    'player_info_container': [`player-info`],
-    'player_name_container': [`player-name`],   
-    'player_position_container': [`player-position`],
-}
-
-const team_container = document.getElementById('team-container');
-
-fragment_players = new DocumentFragment();
-
-players_details.forEach( player => {
-
-    //Creates Player Card
-    const player_card = document.createElement('div');
-    player_card.classList.add(...player_card_classes.player_card_container);
-
-    //Creates Player image
-    const player_image = document.createElement('img');
-    player_image.setAttribute('src', player.player_pic);
-
-    player_card.appendChild(player_image);
-
-    //Creates player info
-    const player_info = document.createElement('div');
-    player_info.classList.add(...player_card_classes.player_info_container);
-
-    const player_name = document.createElement('div');
-    player_name.classList.add(...player_card_classes.player_name_container);
-    player_name.textContent = player.name;
-
-    const player_position = document.createElement('div');
-    player_position.classList.add(...player_card_classes.player_position_container);
-    player_position.textContent = `#${player.number} ${player.position}`;
-
-    player_info.appendChild(player_name);
-    player_info.appendChild( player_position);
-
-    player_card.appendChild(player_info);
-
-    //Append all components
-    fragment_players.appendChild(player_card);
-});
-
-team_container.appendChild(fragment_players);
---
+++ js/data.js
@@ -0,0 +1,218 @@
+/*Navbar items declarions: menu items texts + links */
+export const navbar_items = [
+    {'item_name': 'Scores', 'link': '#', 'class':'navbar-item'},
+    {'item_name': 'Team', 'link': '#', 'class':'navbar-item'},
+    {'item_name': 'Schedule', 'link': '#', 'class':'navbar-item'},
+    {'item_name': 'Live', 'link': '#', 'class':'navbar-item'},
+    {'item_name': 'Store', 'link': '#', 'class':'navbar-item'},
+];
+
+
+/*Scores*/
+/*Scores from previous matches*/
+export const score_details = [
+    {'date': 'Final', 
+    'score_local':'108',
+    'score_visit':'111',
+    'logo_local': './media/MIA-viceversa.png',
+    'logo_visit': './media/LAL.png',
+    'location': 'Miami'
+    },
+    {'date': 'Final - extra time', 
+    'score_local':'110',
+    'score_visit':'111',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Denver'
+    },
+    {'date': 'Final', 
+    'score_local':'99',
+    'score_visit':'85',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Chicago'
+    },
+    {'date': 'Final', 
+    'score_local':'108',
+    'score_visit':'111',
+    'logo_local': './media/MIA-viceversa.png',
+    'logo_visit': './media/LAL.png',
+    'location': 'Miami'
+    },
+    {'date': 'Final - extra time', 
+    'score_local':'110',
+    'score_visit':'111',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Denver'
+    },
+    {'date': 'Final', 
+    'score_local':'99',
+    'score_visit':'85',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Chicago'
+    },
+    {'date': 'Final', 
+    'score_local':'108',
+    'score_visit':'111',
+    'logo_local': './media/MIA-viceversa.png',
+    'logo_visit': './media/LAL.png',
+    'location': 'Miami'
+    },
+    {'date': 'Final - extra time', 
+    'score_local':'110',
+    'score_visit':'111',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Denver'
+    },
+    {'date': 'Final', 
+    'score_local':'99',
+    'score_visit':'85',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Chicago'
+    },
+];
+
+export const score_card_classes = {
+    'game_card_conteiner': [`match-score`, `game-card`, `flex`],
+    'game_date': [`match-date`],
+    'game_score_detail': [`score-detail`, `flex`],
+    'game_location': [`match-location`, `flex`],
+}
+
+
+/*Schedule*/
+/*Schedule for next matches*/
+export const schedule_details = [
+    {'date': 'Monday - April 26th, 2021', 
+    'score_local':'108',
+    'score_visit':'111',
+    'logo_local': './media/MIA-viceversa.png',
+    'logo_visit': './media/LAL.png',
+    'location': 'Miami'
+    },
+    {'date': 'Tuesday - April 27th, 2021', 
+    'score_local':'-',
+    'score_visit':'-',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Denver'
+    },
+    {'date': 'Friday - April 30th, 2021', 
+    'score_local':'-',
+    'score_visit':'-',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Chicago'
+    },
+    {'date': 'Monday - April 26th, 2021', 
+    'score_local':'108',
+    'score_visit':'111',
+    'logo_local': './media/MIA-viceversa.png',
+    'logo_visit': './media/LAL.png',
+    'location': 'Miami'
+    },
+    {'date': 'Tuesday - April 27th, 2021', 
+    'score_local':'-',
+    'score_visit':'-',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Denver'
+    },
+    {'date': 'Friday - April 30th, 2021', 
+    'score_local':'-',
+    'score_visit':'-',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Chicago'
+    },
+    {'date': 'Monday - April 26th, 2021', 
+    'score_local':'108',
+    'score_visit':'111',
+    'logo_local': './media/MIA-viceversa.png',
+    'logo_visit': './media/LAL.png',
+    'location': 'Miami'
+    },
+    {'date': 'Tuesday - April 27th, 2021', 
+    'score_local':'-',
+    'score_visit':'-',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Denver'
+    },
+    {'date': 'Friday - April 30th, 2021', 
+    'score_local':'-',
+    'score_visit':'-',
+    'logo_local': './media/LAL.png',
+    'logo_visit': './media/MIA-viceversa.png',
+    'location': 'Chicago'
+    },
+];
+
+export const schedule_card_classes = {
+    'game_card_conteiner': [`match-score`, `game-card`, `flex`],
+    'game_date': [`match-date`],
+    'game_score_detail': [`score-detail`, `flex`],
+    'game_location': [`match-location`, `flex`],
+}
+
+
+/*Team*/
+/*Team Players Info*/
+export const players_details = [
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+    {'name': 'Nemanja Bjelica', 
+    'position':'Center-Forward',
+    'number':'70',
+    'player_pic': './media/players/roster-vv-1628389.png',
+    },
+];
+
+export const player_card_classes = {
+    'player_card_container': [`player-card`,  `flex`],
+    'player_info_container': [`player-info`],
+    'player_name_container': [`player-name`],   
+    'player_position_container': [`player-position`],
+}
